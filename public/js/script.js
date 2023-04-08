020df5977d6181f5b5f142192cfef332ab894a0e
Jorge Mendez <jorgemendez3421@gmail.com>
2023-04-08 13:33:50 -0700

    Color of interactive links with JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,8 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 navToggle.addEventListener('click', (event) => {
     const visibility = primaryNav.getAttribute('data-visible');
 
+    // console.log(visibility);
+    
     if(visibility === "false"){
         primaryNav.setAttribute('data-visible', true)
         navToggle.setAttribute('aria-expanded', true)
@@ -13,4 +15,14 @@ navToggle.addEventListener('click', (event) => {
         primaryNav.setAttribute('data-visible', false)
         navToggle.setAttribute('aria-expanded', false)
     }
-})
+
+    // si el menu esta minimizado el color sera blanco y negro cuando se enfoque
+    const navLinks = primaryNav.querySelectorAll('a');
+    navLinks.forEach(link =>{
+        if(navToggle.getAttribute('aria-expanded') === "true"){
+            link.style.color = 'white';
+        } else {
+            link.style.color = 'black';
+        }
+    });
+});
